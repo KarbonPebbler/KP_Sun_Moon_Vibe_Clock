5e53db44c3a585c5c96d66b39be0d75d091a7759
antgiant <antgiant@gmail.com>
2013-07-13 15:59:51 -0400

    Add dummy location. (Roughly the North Pole).

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 //NOTE: longitude is positive for East and negative for West
-#define LATITUDE    XX.XXXX
-#define LONGITUDE -X.XXXXX
-#define TIMEZONE 0;
+#define LATITUDE    85
+#define LONGITUDE 0
+#define TIMEZONE 0.0;
 //NOTE: Change false to true if you want to enable the vibe function
 #define HOUR_VIBRATION false
