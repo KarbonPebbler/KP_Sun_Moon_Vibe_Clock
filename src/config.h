a3ca3b6b42012821488f26fb4f7d1d994ba572fc
alextheswimmer <alexlong0331@gmail.com>
2014-01-02 18:38:28 -0500

    Update config.h
    For Tampa fl

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 //NOTE: longitude is positive for East and negative for West
-#define LATITUDE    XX.XXXX
-#define LONGITUDE -X.XXXXX
+#define LATITUDE    28.2075
+#define LONGITUDE -82.43827
 #define TIMEZONE 0;
 //NOTE: Change false to true if you want to enable the vibe function
 #define HOUR_VIBRATION false
